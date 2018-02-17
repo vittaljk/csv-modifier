ae6a67a7dc97e2ab2b1522873f4a3dcb7662e61c
vijay vittal <vijay.vittal@votercircle.com>
2018-02-18 00:44:32 +0530

    download csv done!!!

--- src/app/components/drap-demo/drap-demo.component.ts
+++ src/app/components/drap-demo/drap-demo.component.ts
@@ -12,19 +12,26 @@ export class DrapDemoComponent implements OnInit {
 
     constructor(private papa: PapaParseService) {}
 
-    ngOnInit() { }
+    ngOnInit() {}
 
     readFile(event): void {
         this.papa.parse(event.target.files[0], {
-            complete: (result) => {
+            complete: result => {
                 this.simpleList.push(result.data);
-                const csv = this.papa.unparse(result.data);
-                console.log(csv);
             }
         });
     }
 
     removeItem(item: any, list: any[]): void {
         list.splice(list.indexOf(item), 1);
+    }
+
+    download(): void {
+        const csvString = this.papa.unparse(this.simpleList[0]);
+        const blob = new Blob([csvString], { type: 'text/csv' });
+        const url = window.URL.createObjectURL(blob);
+        window.open(url);
+        // or
+        // window.open(window.URL.createObjectURL(new Blob([this.papa.unparse(this.simpleList[0])], { type: 'text/csv' })));
     }
 }
